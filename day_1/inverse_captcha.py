4e59c57f25dacd0fcbfd20e6af710f218fc3a483
haakensonb <untamedhawk44@gmail.com>
2017-12-14 16:12:33 -0500

    captcha_2 completed

--- day_1/inverse_captcha.py
+++ day_1/inverse_captcha.py
@@ -5,8 +5,19 @@ f.closed
 def captcha_1(nums):
     sum = 0
     for i in range(len(nums)):
+        # Modulus is needed to keep index within the circular list
         if nums[i] == nums[(i+1) % len(nums)]:
             sum += int(nums[i])
     return sum
 
 print(captcha_1(puzzle_data))
+
+def captcha_2(nums):
+    sum = 0
+    for i in range(len(nums)):
+        # Check number halfway around circular list
+        if nums[i] == nums[(i+(len(nums)//2)) % len(nums)]:
+            sum += int(nums[i])
+    return sum
+
+print(captcha_2(puzzle_data))
