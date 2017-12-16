0cb5cad5b89f838a1c1e030d8b0b15d93043fc56
haakensonb <untamedhawk44@gmail.com>
2017-12-15 20:21:25 -0500

    Part one completed

---
+++ day_2/corruption_checksum.py
@@ -0,0 +1,16 @@
+# spreadsheet = '5195 753 2468'
+
+def format_input():
+    with open('./day_2/input.txt') as f:
+        spreadsheet = f.read().strip().splitlines()
+    f.closed
+    return [[int(x) for x in row.split()] for row in spreadsheet]
+
+def checksum(spreadsheet):
+    sum = 0
+    for row in spreadsheet:
+        sum += max(row) - min(row)
+    return sum
+
+spreadsheet = format_input()
+print(checksum(spreadsheet))
