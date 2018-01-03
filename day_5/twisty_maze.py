f839a23eb5fac25b1b1708a6c6ba63157e9e0731
haakensonb <untamedhawk44@gmail.com>
2018-01-03 16:29:38 -0500

    Part 2 Completed

--- day_5/twisty_maze.py
+++ day_5/twisty_maze.py
@@ -4,6 +4,8 @@ with open('./day_5/input.txt') as f:
 f.close
 maze = [int(line.strip()) for line in maze]
 
+# I think I will just expand on the original functions
+# from now on rather than separating part one from two
 def escape_maze(maze):
     end = len(maze) - 1
     step_counter = 0
@@ -12,9 +14,13 @@ def escape_maze(maze):
     while True:
         cur_index = offset
         offset = offset + maze[cur_index]
-        maze[cur_index] = maze[cur_index] + 1
+        if maze[cur_index] >= 3:
+            maze[cur_index] = maze[cur_index] - 1
+        else:
+            maze[cur_index] = maze[cur_index] + 1
         step_counter += 1
         if offset > end:
             return step_counter
+
 
 print(escape_maze(maze))
